ddf03c49a18021f40bd49394ccae17b141b455ed
e-albaugh <eric.albaugh@microfocus.com>
2025-03-07 17:00:04 -0500

    mocha reporter add

--- cypress.config.js
+++ cypress.config.js
@@ -1,39 +1,34 @@
 // cypress.config.js
 const { defineConfig } = require('cypress');
 const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
-const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
-const { createEsbuildPlugin } =
-  require('@badeball/cypress-cucumber-preprocessor/esbuild');
+const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor');
+const { createEsbuildPlugin } = require('@badeball/cypress-cucumber-preprocessor/esbuild');
 
 module.exports = defineConfig({
   e2e: {
     async setupNodeEvents(on, config) {
-      // Set up esbuild bundler for .feature files
+      // Preprocessor for Gherkin
       const bundler = createBundler({
         plugins: [createEsbuildPlugin(config)],
       });
       on('file:preprocessor', bundler);
 
-      // Add the cucumber preprocessor plugin WITH JSON output
-      await addCucumberPreprocessorPlugin(on, config, {
-        // This tells the preprocessor to produce a JSON report
-        json: {
-          enabled: true,
-          output: 'results/cucumber-report.json', 
-          // You can pick any output path you like
-        },
+      // Enable the cucumber preprocessor
+      await addCucumberPreprocessorPlugin(on, config);
 
-        // Optionally, if you want "messages" output:
-        // messages: {
-        //   enabled: true,
-        //   output: 'results/cucumber-messages.ndjson'
-        // }
-      });
-
-      // Return updated config
+      // Return config
       return config;
     },
-    specPattern: '**/*.feature', // Tells Cypress to treat .feature files as specs
-    supportFile: false
+
+    // IMPORTANT: Use mocha-junit-reporter
+    reporter: 'mocha-junit-reporter',
+    reporterOptions: {
+      mochaFile: 'results/test-results-[hash].xml',
+      toConsole: true
+    },
+
+    // We'll look for .feature specs
+    specPattern: '**/*.feature',
+    supportFile: false,
   }
 });
